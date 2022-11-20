34120ce774a096d65b41388798e3d050f03dbe8a
François Rlt <francois.rault@imt-atlantique.net>
2022-11-20 15:39:48 +0100

    [feat] :sparkles: Update DeleteAccount Modal

--- src/components/modal/DeleteAccount.vue
+++ src/components/modal/DeleteAccount.vue
@@ -5,26 +5,60 @@
     >
       <div class="row pt-3">
         <div class="col">
-          <h4>Un problème pour enregistrer votre carte VA? &#128557;</h4>
+          <h4>Vous souhaitez nous quitter? &#128557;</h4>
         </div>
       </div>
-      <div class="row pt-3">
-        <div class="col m-2 text-start">
-          <p>
-            Pas de panique, envoyer un petit e-mail à test@gmail.com en
-            indiquant:
+      <div class="row pt-3 px-2">
+        <div class="col mx-2 text-start">
+          <p  >
+            <strong><u>Attention!</u></strong> La suppression de votre compte
+            entrainera la suppression de toutes vos données personnelles de
+            manière irreverssible. Il ne vous sera plus possible de vous
+            connecter et d'accéder notamment à vos factures.
           </p>
-          <ul class="ms-5">
-            <li class="fw-bold">Votre pseudo</li>
-            <li class="fw-bold">Votre nom</li>
-            <li class="fw-bold">Votre prenom</li>
-            <li class="fw-bold">Votre numéro de carte VA</li>
-          </ul>
           <p>
-            Attendez la validation de votre carte VA avant de procéder au
-            payement (passez l'étape)... du moins si vous souhaitez bénéficier
-            de la réduction associée &#128521;.
+            Pour a nouveau participer aux courses, il faudra vous reinscrire et
+            repayer les droits d'admission (sauf derogation).
           </p>
+          <p>
+            Si vous souhaitez poursuivre la suppression de votre compte,
+            veuillez renseignez votre mot de passe ci-dessous, puis cliquez sur
+            le button supprimer.
+          </p>
+        </div>
+      </div>
+      <div class="row text-start">
+        <div class="col m-2">
+          <div class="row px-2 fw-bold" v-show="wrongPassword">
+            <div class="col text-danger">Mauvais mot de passe</div>
+          </div>
+          <form action="">
+            <div class="row p-2 fw-bold">
+              <div class="col form-group">
+                <label for="inputNewPassword"> Mot de passe:</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="inputNewPassword"
+                  v-model="password"
+                  minlength="8"
+                  required
+                />
+              </div>
+            </div>
+
+            <div class="row mt-4">
+              <div class="col form-group text-center">
+                <button
+                  class="btn btn-danger"
+                  type="button"
+                  @click="deleteAccount"
+                >
+                  Supprimer
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -34,9 +68,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
+  data() {
+    return {
+      password: "",
+      wrongPassword: false,
+    };
+  },
   methods: {
     closeModal() {
+      this.password = "";
+      this.wrongPassword = false;
       this.$emit("closeDeleteAccountModal");
+    },
+    deleteAccount() {
+      console.log("Delete Account.");
     },
   },
 });
