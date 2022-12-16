23ef2b73a263c9fb5013c92b8ca53f09acced721
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2022-12-16 15:13:10 +0100

    chore(axios): :art: update some axios things

--- src/axios.ts
+++ src/axios.ts
@@ -2,17 +2,16 @@ import axios from "axios";
 import { MutationTypes } from "./store/modules/auth";
 import store from "./store";
 
-axios.defaults.baseURL = "https://overrun.24heures.org/api/v1/";
+axios.defaults.baseURL = "https://overrun.traefik.me/api/v1/";
 
 axios.defaults.withCredentials = true;
 
 
-// axios.interceptors.response.use(
-//     (res) => {
-//         return res;
-//     }, 
-//     async (err) => {
-//         const originalConfig = err.config;
-//         if(originalConfig)
-//     }
-// )
+axios.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    async (err) => {
+        return err.response;
+    }
+)
