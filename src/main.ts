3fc021ed325c6aa844da77d62c6f942eb8b474c0
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2022-12-16 15:15:12 +0100

    style: :fire: remove some statics stuffs

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,4 @@ console.log(axios.defaults.baseURL);
 
 export const edition = 1;
 
-store.commit(
-  MutationTypes.SET_ACCESS_TOKEN,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwidXNlcm5hbWUiOiJUZXN0UHNldWRvIiwicm9sZSI6IkFUSExFVEUiLCJleHBpcmVfYXQiOiIyMDIyLTAyLTE4VDAwOjE2OjU3LjQwOFoiLCJhdGhsZXRlSWQiOjUsImFkbWluSWQiOm51bGwsImlhdCI6MTY0NTExMzQxNywiZXhwIjoxNjQ1MTQzNDE3fQ.HaarXG7GgF0WLTF3aUOyDjLGn90LvPqMcs8WPun6m7A"
-);
-
 createApp(App).use(router).use(store).mount("#app");
