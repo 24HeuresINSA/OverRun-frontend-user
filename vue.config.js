d8e8c0445e3b493ce66bc4e5f6acffd05615b40d
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2023-01-08 16:11:22 +0100

    refactor: :rocket: auto switch between dev and prod url

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
-process.env.VUE_APP_API_BASE_URL = "https://overrun.24heures.org/api/v1";
+process.env.VUE_APP_API_BASE_URL = `https://overrun.${
+  process.env.NODE_ENV === "production" ? "24heures.org" : "traefik.me"
+}/api/v1`;
 
 module.exports = {
   devServer: {
     disableHostCheck: true,
-    public: "https://overrun.24heures.org",
+    public: `https://overrun.${
+      process.env.NODE_ENV === "production" ? "24heures.org" : "traefik.me"
+    }`,
   },
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
