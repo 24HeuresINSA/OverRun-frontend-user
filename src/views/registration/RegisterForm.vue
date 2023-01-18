68af36e13d5286191c6341b4d8bb91f8608d62db
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2023-01-18 22:50:08 +0100

    style: :pencil2: change agrrement

--- src/views/registration/RegisterForm.vue
+++ src/views/registration/RegisterForm.vue
@@ -194,7 +194,7 @@
                 required
               />
               <label for="checkRules">
-                Je confirme que j'ai bien pris connaissance du
+                J'ai lu et j'accepte le
                 <a
                   href="https://www.24heures.org/wp-content/uploads/2022/01/2022-01-19_Reglement-Interieur-Courses.pdf"
                   >règlement en vigueur</a
